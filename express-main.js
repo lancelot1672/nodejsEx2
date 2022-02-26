1df32d43f5eb96275c84f2ae3e1889db6e82b54f
김동률 <dore@gimdonglyul-ui-MacBookAir.local>
2022-02-26 20:57:28 +0900

    readdir 모듈만들어 사용

--- express-main.js
+++ express-main.js
@@ -15,36 +15,41 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended : false}));
 
-app.get('/', function(request, response){
+// 모듈 만들어서 사용하기
+app.use(function(request, response, next){
     fs.readdir('./data', function(error, filelist){
-        console.log(filelist)
-        var title = 'Welcome';
-        var description = 'Hello, Node.js';
+        request.list= filelist;
+        next();
+    });
+});
 
-        var list = template.list(filelist);
-        var html = template.HTML(title, list,
-            `<h2>${title}</h2>${description}`,
-            `<a href="/topic/create">createPage</a>`
-            );
-            response.send(html);
-    })
+app.get('/', function(request, response){  
+    var title = 'Welcome';
+    var description = 'Hello, Node.js';
+
+    var list = template.list(request.list);
+    var html = template.HTML(title, list,
+        `<h2>${title}</h2>${description}`,
+        `<a href="/topic/create">createPage</a>`
+        );
+        response.send(html);
+
 });
 app.get('/topic/create', function(request, response){
-    fs.readdir('./data', function(error, filelist){
-        var title = 'WEB - create';
-        var list = template.list(filelist);
-        var html = template.HTML(title, list,
-            `<form action = "/topic/create_process" method="post">
-                <p><input type="text" name="title" placeholder="title"></p>
-                <p>
-                    <textarea name="description" placeholder="description"></textarea>
-                </p>
-                <p>
-                    <input type="submit">
-                </p>
-            </form>`);
-        response.send(html);
-    });
+    var title = 'WEB - create';
+    var list = template.list(request.list);
+    var html = template.HTML(title, list,
+        `<form action = "/topic/create_process" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+                <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+                <input type="submit">
+            </p>
+        </form>`);
+    response.send(html);
+
 });
 app.post('/topic/create_process', function(request, response){
     var post = request.body;
@@ -77,61 +82,57 @@ app.post('/topic/update_process', function(request, response){
     });
 });
 app.get('/topic/update/:pageId', function(request, response){
-    fs.readdir('./data', function(error, filelist){
-        var filename = path.parse(request.params.pageId).base;
+    var filename = path.parse(request.params.pageId).base;
 
-        fs.readFile(`data/${filename}`, function(error, description){
-            if(error){
-                next(error);
-            }else{
-                var title = request.params.pageId;
-                var list = template.list(filelist);
-                var html = template.HTML(title, list,
-                    `<form action = "/topic/update_process" method="post">
-                        <input type="hidden" name="id" value="${title}">
-                        <p><input type="text" name="title" placeholder="title" value="${title}">
-                        <p>
-                            <textarea name="description" placeholder="description">${description}</textarea>
-                        </p>
-                        <p>
-                            <input type="submit">
-                        </p>
-                    </form>
-                    `,
-                    `<a href="/page/create">createPage</a> 
-                    <br> 
-                    <a href="/page/update/${title}">update</a>`
-                    );
-                response.send(html);
-            }
-        });
+    fs.readFile(`data/${filename}`, function(error, description){
+        if(error){
+            next(error);
+        }else{
+            var title = request.params.pageId;
+            var list = template.list(request.list);
+            var html = template.HTML(title, list,
+                `<form action = "/topic/update_process" method="post">
+                    <input type="hidden" name="id" value="${title}">
+                    <p><input type="text" name="title" placeholder="title" value="${title}">
+                    <p>
+                        <textarea name="description" placeholder="description">${description}</textarea>
+                    </p>
+                    <p>
+                        <input type="submit">
+                    </p>
+                </form>
+                `,
+                `<a href="/page/create">createPage</a> 
+                <br> 
+                <a href="/page/update/${title}">update</a>`
+                );
+            response.send(html);
+        }
     });
 });
 
 app.get(`/topic/:pageId`, function(request, response, next){
     //id값이 있는 경우 코드 //코드 순서 되게 중요!!!
-    fs.readdir('./data', function(error, filelist){
-        var fileReadId = path.parse(request.params.pageId).base;
+    var fileReadId = path.parse(request.params.pageId).base;
 
-        fs.readFile(`data/${fileReadId}`, function(error, description){
-            if(error){
-                next(error);
-            }else{
-                var title = request.params.pageId;
-                var list = template.list(filelist);
-                var html = template.HTML(title, list,
-                    `<h2>${title}</h2>${description}`,
-                    `
-                    <a href="/topic/create">createPage</a>
-                    <a href="/topic/update/${title}">update</a>
-                    <form action="/topic/delete_process" method="post">
-                        <input type="hidden" name="id" value="${title}">
-                        <input type="submit" value="delete">
-                    </form>
-                    `);
-                response.send(html);
-            }
-        });
+    fs.readFile(`data/${fileReadId}`, function(error, description){
+        if(error){
+            next(error);
+        }else{
+            var title = request.params.pageId;
+            var list = template.list(request.list);
+            var html = template.HTML(title, list,
+                `<h2>${title}</h2>${description}`,
+                `
+                <a href="/topic/create">createPage</a>
+                <a href="/topic/update/${title}">update</a>
+                <form action="/topic/delete_process" method="post">
+                    <input type="hidden" name="id" value="${title}">
+                    <input type="submit" value="delete">
+                </form>
+                `);
+            response.send(html);
+        }
     })
 })
 app.listen(3000, function(){
